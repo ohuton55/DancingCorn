82fcb66d6b498d5ab193481320ce5e7c16c3b45f
ohuton55 <tttt.tttt.no@gmail.com>
2022-11-01 22:45:10 +0900

    プロジェクト ファイルを追加します。

---
+++ DancingCorn/Form1.Designer.cs
@@ -0,0 +1,145 @@
+﻿namespace DancingCorn
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.dispHideToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.contextMenuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox1.Image = global::DancingCorn.Properties.Resources.compressed3;
+            this.pictureBox1.Location = new System.Drawing.Point(2, 0);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(294, 333);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.dispHideToolStripMenuItem,
+            this.exitToolStripMenuItem});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(147, 52);
+            // 
+            // dispHideToolStripMenuItem
+            // 
+            this.dispHideToolStripMenuItem.Name = "dispHideToolStripMenuItem";
+            this.dispHideToolStripMenuItem.Size = new System.Drawing.Size(146, 24);
+            this.dispHideToolStripMenuItem.Text = "Disp-Hide";
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(146, 24);
+            this.exitToolStripMenuItem.Text = "Exit";
+            // 
+            // notifyIcon1
+            // 
+            this.notifyIcon1.ContextMenuStrip = this.contextMenuStrip1;
+            this.notifyIcon1.Text = "notifyIcon1";
+            this.notifyIcon1.Visible = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(294, 328);
+            this.ControlBox = false;
+            this.Controls.Add(this.pictureBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.Name = "Form1";
+            this.ShowInTaskbar = false;
+            this.Text = "Form1";
+            this.TransparencyKey = System.Drawing.Color.Black;
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.contextMenuStrip1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private PictureBox pictureBox1;
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            int movePixcel = 10;
+            if (e.Alt)
+            {
+                movePixcel = 1;
+            }
+            switch (e.KeyCode)
+            {
+                case Keys.Up:
+                    this.Top -= movePixcel;
+                    break;
+                case Keys.Down:
+                    this.Top += movePixcel;
+                    break;
+                case Keys.Left:
+                    this.Left -= movePixcel;
+                    break;
+                case Keys.Right:
+                    this.Left += movePixcel;
+                    break;
+            }
+        }
+
+        private ContextMenuStrip contextMenuStrip1;
+        private ToolStripMenuItem dispHideToolStripMenuItem;
+
+        private void dispHideToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Visible = !Visible;  //Formの表示、非表示を切り替える
+        }
+
+        private ToolStripMenuItem exitToolStripMenuItem;
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private NotifyIcon notifyIcon1;
+    }
+}
---
+++ DancingCorn/Form1.cs
@@ -0,0 +1,10 @@
+namespace DancingCorn
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
